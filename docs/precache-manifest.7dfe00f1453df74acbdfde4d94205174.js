c7e7c4f51a404ef8be13d62659e0b14ff72e6989
andreicristea <andrei.cristea@apicore.de>
2020-04-29 15:47:17 +0300

    Production folder renamed to docs

---
+++ docs/precache-manifest.7dfe00f1453df74acbdfde4d94205174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fa7694a2bfbff3c07670dee9d12d2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc92261e47c73577587f",
+    "url": "/static/css/main.8c979fc8.chunk.css"
+  },
+  {
+    "revision": "0d59a42c41e343356975",
+    "url": "/static/js/2.0e7537d0.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.0e7537d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc92261e47c73577587f",
+    "url": "/static/js/main.d771f914.chunk.js"
+  },
+  {
+    "revision": "e65e630bdd6bc99d927c",
+    "url": "/static/js/runtime-main.27a68170.js"
+  }
+]);
