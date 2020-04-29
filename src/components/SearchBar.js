c8a1470805bd883ff328b7ad575630e90105d64c
andreicristea <andrei.cristea@apicore.de>
2020-04-29 11:05:47 +0300

    Aplication completed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,29 @@
 import React from "react";
 
 class SearchBar extends React.Component{
+    state = {term: ''};
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        this.props.onTermSubmit(this.state.term);
+    };
+
+    onInputChange = (e) => {
+        this.setState({term: e.target.value});
+    };
+
     render() {
         return (
-            <div>
-                <label htmlFor={"search"}>Search</label>
-                <input id={"search"} type={"text"}/>
+            <div className={"ui segment"}>
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label htmlFor={"search"}>Video Search</label>
+                        <input id={"search"} type={"text"}
+                               value={this.state.term}
+                               onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
             </div>
         )
     }
